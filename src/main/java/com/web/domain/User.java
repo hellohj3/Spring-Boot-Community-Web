04d7ae7972dc58d2836963d78169018a42e6aa64
Myhyem <hellohj_@naver.com>
2021-03-06 20:38:22 +0900

    소셜로그인 완료(카카오안됨 미친)

--- src/main/java/com/web/domain/User.java
+++ src/main/java/com/web/domain/User.java
@@ -1,13 +1,11 @@
 package com.web.domain;
 
+import com.web.domain.enums.SocialType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -23,15 +21,22 @@ public class User implements Serializable {
     private String name;
     private String password;
     private String email;
+    private String principal;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
 
     @Builder
-    public User(Long idx, String name, String password, String email, LocalDateTime createdDate, LocalDateTime updatedDate) {
+    public User(Long idx, String name, String password, String email, String principal, SocialType socialType, LocalDateTime createdDate, LocalDateTime updatedDate) {
         this.idx = idx;
         this.name = name;
         this.password = password;
         this.email = email;
+        this.principal = principal;
+        this.socialType = socialType;
         this.createdDate = createdDate;
         this.updatedDate = updatedDate;
     }
